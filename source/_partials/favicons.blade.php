1b07f30422753ecd5831887d63fc45f002905fc4
Pierce McGeough <pmcgeough@hotmail.co.uk>
2023-01-01 23:10:56 +0000

    Update favicon

--- source/_partials/favicons.blade.php
+++ source/_partials/favicons.blade.php
@@ -1,5 +1,5 @@
-<link rel="shortcut icon" href="./assets/images/icon/favicon.png">
-<link rel="apple-touch-icon-precomposed" sizes="144x144" href="./assets/images/icon/apple-touch-icon-144-precomposed.png">
-<link rel="apple-touch-icon-precomposed" sizes="114x114" href="./assets/images/icon/apple-touch-icon-114-precomposed.png">
-<link rel="apple-touch-icon-precomposed" sizes="72x72" href="./assets/images/icon/apple-touch-icon-72-precomposed.png">
-<link rel="apple-touch-icon-precomposed" href="./assets/images/icon/apple-touch-icon-57-precomposed.png">
+<link rel="shortcut icon" href="./assets/images/mugendo/mai-logo.png">
+<link rel="apple-touch-icon-precomposed" sizes="144x144" href="./assets/images/mugendo/mai-logo.png">
+<link rel="apple-touch-icon-precomposed" sizes="114x114" href="./assets/images/mugendo/mai-logo.png">
+<link rel="apple-touch-icon-precomposed" sizes="72x72" href="./assets/images/mugendo/mai-logo.png">
+<link rel="apple-touch-icon-precomposed" href="./assets/images/mugendo/mai-logo.png">
